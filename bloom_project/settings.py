d05b5cfc28fb7364ff6db74a22d552a32d519900
anishthapa66 <anishthapa6699@gmail.com>
2025-07-24 01:42:58 +0545

    finishing

--- bloom_project/settings.py
+++ bloom_project/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here')
 
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
 
